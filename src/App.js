78806ee73aea6b9d620986315699553a083f0df9
amarborz <amarborz@hotmail.com>
2022-04-07 02:04:19 +0300

    demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+
+// images
+import moon from './assets/images/moon.png'
+import mountainsBack from './assets/images/mountains_behind.png'
+import mountainsFront from './assets/images/mountains_front.png'
+import stars from './assets/images/stars.png'
+
+// Rellax
+import Parallax from 'react-rellax'
+
+import About from './components/About'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<div className="parallax">
+				<div className="text">
+					<h1>A Short Story...</h1>
+				</div>
+				<Parallax speed={-5}>
+					<img src={moon} alt="moon" />
+				</Parallax>
+				<Parallax speed={1}>
+					<img src={mountainsBack} alt="mountains" />
+				</Parallax>
+				<Parallax speed={-0.2}>
+					<img src={mountainsFront} alt="mountains" />
+				</Parallax>
+				<img src={stars} alt="stars" />
+			</div>
+			<div className="about">
+				<About />
+			</div>
+		</>
+	)
 }
 
-export default App;
+export default App
---
+++ src/components/About.jsx
@@ -0,0 +1,13 @@
+const About = () => {
+	return (
+		<div>
+			<h1>Once upon a time...</h1>
+			<p>
+				There was a man who roamed the world at night. Nothing did he search,
+				and nothing he found. The end.
+			</p>
+		</div>
+	)
+}
+
+export default About
